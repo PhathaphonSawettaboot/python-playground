e556e01ce71b666c43ffe53730dee4808695c502
Phathaphon Sawettaboot <132444206+PhathaphonSawettaboot@users.noreply.github.com>
2023-08-12 19:35:26 +0700

    even-or-odd-program

--- python-learning-lab.py
+++ python-learning-lab.py
@@ -1,21 +1,9 @@
-def calculate_bmi(weight , height):
-    bmi = weight / (height ** 2)
-    return bmi
+x = 10
+y = 20
 
-def bmi_status(bmi):
-    if bmi < 18.5:
-        return "Underweight"
-    elif 18.5 <= bmi < 24.9:
-        return "Normal"
-    elif 25 <= bmi < 29.9:
-        return "Overweight"
-    else:
-        return "Obese"
-
-weight = float(input("Enter yout weight in kilograms: "))
-height = float(input("Enyter your height in meters: "))
-
-bmi = calculate_bmi(weight, height)
-status = bmi_status(bmi)
-print(f"Your BMI is: {bmi:.3f}")
-print(f"Your BMI status is: {status}")
+if x > 5:
+    print("x greater than 5")
+    if y > 15:
+        print("y is also geater than 15")
+else:
+    print("x is not greater than 5")
