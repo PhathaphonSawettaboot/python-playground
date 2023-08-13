e7a39690eb78d69804dbe22a72668adfc354156f
Phathaphon Sawettaboot <132444206+PhathaphonSawettaboot@users.noreply.github.com>
2023-08-13 18:32:31 +0700

    transfer

--- python-learning-lab.py
+++ python-learning-lab.py
@@ -1,6 +1,11 @@
-def greet(name):
-    """
-    This function greets the person passed in as a parameter.
-    """
-    print(f"Hello, {name}!")
-greet("La-la")
+try:
+    number = int(input("Enter an integer: "))
+    result = 10 / number
+except ValueError:
+    print("Invalid input. Please enter an integer.")
+except ZeroDivisionError:
+    print("Cannot divide by zero.")
+else:
+    print(f"Result: {result}")
+finally:
+    print("Exception handling complete.")
---
+++ transfer-deposit-withdrawl.py
@@ -0,0 +1,43 @@
+class BankAccount:
+    def __init__(self, account_number, balance):
+        self.account_number = account_number
+        self.balance = balance
+
+    def deposit(self, amount):
+        self.balance = self.balance + amount
+        print(f"Deposit {amount} units. New balance: {self.balance}")
+    
+    def withdraw(self, amount):
+        if self.balance >= amount:
+            self.balance = self.balance - amount
+            print(f"Deposit {amount} units. New balance: {self.balance}")
+        else:
+            print("Insufficient funds.")
+    
+    def transfer(self, other_account, amount):
+        if self.balance >= amount:
+            self.balance = self.balance - amount
+            other_account.balance = other_account.balance + amount
+            print(f"Tranferred {amount} units to {other_account.account_number}. Your new balance: {self.balance}")
+        else:
+            print("Insufficient funds.")
+
+account1 = BankAccount("123456", 1000)
+account2 = BankAccount("789012", 2000)
+
+while True:
+    print("Select an action:")
+    print("1. Transfer")
+    print("2. Deposit")
+    print("3. Withdraw")
+    print("4. Quit")
+    choice = int(input("Enter your choice: "))
+
+    if choice == 1:
+        amount = float(input("Enter the amount to transfer: "))
+        account_number = input("Enter the recipient's account number: ")
+        if account_number == account2.account_number:
+            account1.transfer(account2, amount)
+        else:
+            print("Recipient account not found.")
+             
