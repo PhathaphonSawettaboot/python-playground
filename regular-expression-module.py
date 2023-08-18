37826465fc7d29eba69d096f558ff052872a73ed
Phathaphon Sawettaboot <132444206+PhathaphonSawettaboot@users.noreply.github.com>
2023-08-18 12:45:18 +0700

    Phone Number Validation and Data Extraction

--- regular-expression-module.py
+++ regular-expression-module.py
@@ -1,3 +1,4 @@
+# Email Validation
 import re
 
 email = "la-la@email.com"
@@ -5,3 +6,23 @@ if re.match(r'^[\w\.-]+@[\w\.-]+$', email):
     print("Valid email")
 else:
     print("Invalid email")
+
+#URL Detection
+import re
+text = "Visit my website at https://www.example.com"
+urls = re.findall(r'http[s]?://(?:[a-zA-Z]|[0-9]|[$-_@.&+]|[!*\\(\\),]|(?:%[0-9a-fA-F][0-9a-fA-F]))+', text)
+print(urls)
+
+#Phone Number Validation
+phone = "123-456-7890"
+if re.match(r'^\d{3}-\d{3}-\d{4}$', phone):
+    print("Valid Phone Number")
+else:
+    print("Invalid Phone Number")
+
+#Data Extraction
+text = "Date: 2023-07-31"
+date = re.search(r'\d{4}-\d{2}-\d{2}', text).group()
+print(date)
+
+
